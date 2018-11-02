934c2dde9cab9d4ff6b2031637adb69645a0ab9e
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-02 17:01:41 -0600

    Adds rotate test

--- test/rotator_test.rb
+++ test/rotator_test.rb
@@ -3,16 +3,21 @@ require './lib/rotator'
 
 class RotatorTests < Minitest::Test
   
+  def setup
+    @character_set = ("a".."z").to_a << " "
+    @rotator = Rotator.new(@character_set)
+  end
+  
   def test_it_exists
-    character_set = ("a".."z").to_a << " "
-    rotator = Rotator.new(character_set)
-    assert_instance_of Rotator, rotator
+    assert_instance_of Rotator, @rotator
   end
   
   def test_it_has_a_character_set
-    character_set = ("a".."z").to_a << " "
-    rotator = Rotator.new(character_set)
-    assert_equal character_set, rotator.character_set
+    assert_equal @character_set, @rotator.character_set
+  end
+  
+  def test_it_can_rotate_a_character
+    assert_equal 'b', @rotator.rotate('a', 1)
   end
   
 end
