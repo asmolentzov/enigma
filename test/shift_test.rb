ab794094a20e6d8e671304ff77134ca76c5d3b69
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-02 17:38:14 -0600

    Adds test for splitting a key

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -3,11 +3,17 @@ require './lib/shift'
 
 class ShiftTest < Minitest::Test
   
-  def test_it_exists
-    shift = Shift.new('12345', '230714')
-    assert_instance_of Shift, shift
+  def setup
+    @shift = Shift.new('12345', '230714')
   end
   
+  def test_it_exists
+    assert_instance_of Shift, @shift
+  end
+  
+  def test_it_can_split_up_key
+    assert_equal [12, 23, 34, 45], @shift.key_split
+  end
   
   
 end
