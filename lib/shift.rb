c13c3c0420d5b50ecbdb32a29ca5170eb37f59b4
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-02 17:53:43 -0600

    Add key_split method

--- lib/shift.rb
+++ lib/shift.rb
@@ -5,4 +5,15 @@ class Shift
     @date = date
   end
   
+  def key_split
+    key_characters = @key.split('')
+    split = []
+    i = 0
+    (@key.length - 1).times do 
+      split << (key_characters[i] + key_characters[i + 1]).to_i
+      i += 1
+    end
+    split
+  end
+  
 end
