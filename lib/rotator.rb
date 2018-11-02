42cb335b56adff7364c37aaeab2e8fe04be92295
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-02 17:04:59 -0600

    Adds rotate method

--- lib/rotator.rb
+++ lib/rotator.rb
@@ -6,4 +6,9 @@ class Rotator
     @character_set = character_set
   end
   
+  def rotate(character, amount)
+    index = @character_set.find_index(character)
+    rotated = @character_set.rotate(amount)
+    rotated[index]
+  end
 end
