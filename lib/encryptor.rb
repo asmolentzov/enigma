5576000b1de967e7ca396517d932a08eac001519
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 16:18:33 -0600

    Expand encrypt method

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,3 +1,5 @@
+require './lib/rotator'
+
 class Encryptor
   
   def initialize(message, shifts)
@@ -5,24 +7,25 @@ class Encryptor
     @shifts = shifts
   end
   
-  # def encrypt
-  #   rotator = Rotator.new(("a".."z").to_a << " ")
-  #   cipher = ''
-  #   character_chunks = @message.four_split
-  #   character_chunks.each do |character_chunk|
-  #     index = 0
-  #     character_chunk.each do |character|
-  #       cipher += rotator.rotate(character, @shifts[index])
-  #       index += 1
-  #     end
-  #   end
-  # end
+  def encrypt
+    rotator = Rotator.new(("a".."z").to_a << " ")
+    cipher = ''
+    character_chunks = four_split
+    character_chunks.each do |character_chunk|
+      index = 0
+      character_chunk.each do |character|
+        cipher += rotator.rotate(character, @shifts[index])
+        index += 1
+      end
+    end
+    cipher
+  end
   
   def four_split
     message_characters = @message.split('')
     split_message = []
     until message_characters == [] do
-      split_message << message_characters.shift(4).join
+      split_message << message_characters.shift(4)
     end
     
     split_message
