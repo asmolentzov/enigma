21762d5c4133a375cce97ceaedb1dfc26c108c75
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 15:36:59 -0600

    Changes test setup

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -1,9 +1,11 @@
-require './test_helper'
+require './test/test_helper'
 require './lib/encryptor'
 
 class EncryptorTest < Minitest::Test
   
   def setup
+    message = "hello world"
+    shifts = [3, 27, 73, 20]
     @encryptor = Encryptor.new(message, shifts)
   end
   
