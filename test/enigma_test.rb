e9d20ccf7bb52436104a4df32bdcee76a1185a93
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 16:55:40 -0600

    Adds tests for when a message is given without key or date

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -20,4 +20,32 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
   end
   
+  def test_it_can_encrypt_a_message_with_no_key
+    result = @enigma.encrypt("hello world", "040895")
+    assert_equal 'keder ohulw', result[:encryption]
+    assert_equal '040895', result[:date]
+    assert_instance_of String, result[:key]
+    assert_equal 5, result[:key].length
+  end
+  
+  def test_it_can_encrypt_a_message_with_no_date
+    date = Date.today.strftime('%d%m%y')
+    expected = {
+                encryption: "keder ohulw",
+                key: "02715",
+                date: date
+              }
+    assert_equal expected, @enigma.encrypt('hello world', '02715')
+  end
+  
+  def test_it_can_encrypt_a_message_with_no_key_or_date
+    result = @enigma.encrypt('hello world')
+    date = Date.today.strftime('%d%m%y')
+    assert_instance_of String, result[:encryption]
+    assert_equal 'hello world'.length, result[:encryption].length
+    assert_instance_of String, result[:key]
+    assert_equal 5, result[:key].length
+    assert_equal date, result [:date]
+  end
+  
 end
