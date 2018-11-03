d50a20f4438eb0e5d1c78291abc5dca76a230bd9
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 15:34:47 -0600

    Adds basic encryptor test

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -3,6 +3,14 @@ require './lib/encryptor'
 
 class EncryptorTest < Minitest::Test
   
+  def setup
+    @encryptor = Encryptor.new(message, shifts)
+  end
+  
+  def test_it_exists
+    assert_instance_of Encryptor, encryptor
+  end
+  
   
   
 end
