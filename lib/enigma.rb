a46af2611c13928d49a8b7db4fa023dca7c55df5
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 17:14:59 -0600

    Adds check to make sure key and date are correctly assigned

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,6 +4,11 @@ require './lib/encryptor'
 class Enigma
   
   def encrypt(message, key = nil, date = Date.today)
+    if key && key.length > 5
+      date = key
+      key = nil
+    end
+    
     shift = Shift.new(key, date)
     shifts = shift.get_shifts
     encryptor = Encryptor.new(message, shifts)
