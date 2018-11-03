fe5a83a9aea7ffcbbb292c75a314cf1a203a542b
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 16:33:12 -0600

    Add encrypt test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,5 +11,13 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
   
+  def test_it_can_encrypt_a_message
+    expected = {
+                encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"
+              }
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
   
 end
