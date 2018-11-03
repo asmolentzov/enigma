e4fb30270c0e9d21ca374a3464c98fb392dba613
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 17:18:33 -0600

    Improve tests to consider date and key variability

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -22,20 +22,20 @@ class EnigmaTest < Minitest::Test
   
   def test_it_can_encrypt_a_message_with_no_key
     result = @enigma.encrypt("hello world", "040895")
-    assert_equal 'keder ohulw', result[:encryption]
+    assert_instance_of String, result[:encryption]
+    assert_equal 'hello world'.length, result[:encryption].length
     assert_equal '040895', result[:date]
     assert_instance_of String, result[:key]
     assert_equal 5, result[:key].length
   end
   
   def test_it_can_encrypt_a_message_with_no_date
+    result = @enigma.encrypt('hello world', '02715')
     date = Date.today.strftime('%d%m%y')
-    expected = {
-                encryption: "keder ohulw",
-                key: "02715",
-                date: date
-              }
-    assert_equal expected, @enigma.encrypt('hello world', '02715')
+    assert_instance_of String, result[:encryption]
+    assert_equal 'hello world'.length, result[:encryption].length
+    assert_equal '02715', result[:key]
+    assert_equal date, result [:date]
   end
   
   def test_it_can_encrypt_a_message_with_no_key_or_date
