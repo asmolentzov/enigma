09c670ac290a26aed8556f800f64c6bfec1e5e26
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 16:40:35 -0600

    Adds encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,14 @@
+require './lib/shift'
+require './lib/encryptor'
+
 class Enigma
   
-  
+  def encrypt(message, key = nil, date = Date.today)
+    shift = Shift.new(key, date)
+    shifts = shift.get_shifts
+    encryptor = Encryptor.new(message, shifts)
+    cipher = encryptor.encrypt
+    {encryption: cipher, key: key, date: date}
+  end
   
 end
