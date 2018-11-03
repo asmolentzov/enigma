86a52de0d125b08f33777ea7dbacdbe11190c2d2
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 16:04:53 -0600

    Adds four_split method

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -6,16 +6,27 @@ class Encryptor
   end
   
   def encrypt
-  #   rotator = Rotator.new(("a".."z").to_a << " ")
-  #   cipher = ''
-  #   character_chunks = @message.four_split
-  #   character_chunks.each do |character_chunk|
-  #     index = 0
-  #     character_chunk.each do |character|
-  #       cipher += rotator.rotate(character, @shifts[index])
-  #       index += 1
-  #     end
-  #   end
-  # end
+    rotator = Rotator.new(("a".."z").to_a << " ")
+    cipher = ''
+    character_chunks = @message.four_split
+    character_chunks.each do |character_chunk|
+      index = 0
+      character_chunk.each do |character|
+        cipher += rotator.rotate(character, @shifts[index])
+        index += 1
+      end
+    end
+  end
+  
+  def four_split
+    message_characters = @message.split('')
+    split_times = message_characters.length / 4
+    split_message = []
+    split_times.times do 
+      split_message << message_characters.shift(4).join
+    end
+    split_message << message_characters.join
+  end
+  
   
 end
