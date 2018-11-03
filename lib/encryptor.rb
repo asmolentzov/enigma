27db98a96c25c81e388d52c6574d8c9b9a4ae883
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 16:13:28 -0600

    Improves four_split method to handle message lengths evenly divisible by four

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -5,27 +5,27 @@ class Encryptor
     @shifts = shifts
   end
   
-  def encrypt
-    rotator = Rotator.new(("a".."z").to_a << " ")
-    cipher = ''
-    character_chunks = @message.four_split
-    character_chunks.each do |character_chunk|
-      index = 0
-      character_chunk.each do |character|
-        cipher += rotator.rotate(character, @shifts[index])
-        index += 1
-      end
-    end
-  end
+  # def encrypt
+  #   rotator = Rotator.new(("a".."z").to_a << " ")
+  #   cipher = ''
+  #   character_chunks = @message.four_split
+  #   character_chunks.each do |character_chunk|
+  #     index = 0
+  #     character_chunk.each do |character|
+  #       cipher += rotator.rotate(character, @shifts[index])
+  #       index += 1
+  #     end
+  #   end
+  # end
   
   def four_split
     message_characters = @message.split('')
-    split_times = message_characters.length / 4
     split_message = []
-    split_times.times do 
+    until message_characters == [] do
       split_message << message_characters.shift(4).join
     end
-    split_message << message_characters.join
+    
+    split_message
   end
   
   
