b979f05a205e17134bc6a4a1d0dae8f4e63c0da1
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-04 13:31:28 -0700

    Adds Key initial tests

---
+++ test/key_test.rb
@@ -0,0 +1,24 @@
+require './test/test_helper'
+
+class KeyTest < Minitest::Test
+    
+    def test_it_exists
+      key = Key.new(key)
+      assert_instance_of Key, key
+    end
+    
+    def test_it_can_get_keys_if_given_key
+      keys = Key.get_keys('12345')
+      expected = [12, 23, 34, 45]
+      assert_equal expected, keys
+    end
+    
+    def test_it_can_get_keys_if_given_nil
+      keys = Key.get_keys(nil)
+      assert_instance_of Array, keys
+      assert_equal 4, keys.length
+      assert_instance_of Integer, keys.first
+      assert_instance_of Integer, keys.last
+    end
+  
+end
