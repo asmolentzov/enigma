76b9d7cd3fa17cc0c72e0dfc45109792375e59be
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-04 12:35:07 -0700

    Adds writing decryption to new file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -6,5 +6,15 @@ write_filepath = ARGV[1]
 key = ARGV[2]
 date = ARGV[3]
 
-read_file = File.new(read_filepath, 'r')
-require 'pry'; binding.pry
+read_file = File.open(read_filepath, 'r')
+cipher = read_file.read
+
+enigma = Enigma.new
+decryption = enigma.decrypt(cipher, key, date)
+
+write_file = File.open(write_filepath, 'w')
+write_file.write(decryption[:decryption])
+write_file.close
+
+puts "Created '#{write_filepath}' with the key #{decryption[:key]} and date #{decryption[:date]}"
+
