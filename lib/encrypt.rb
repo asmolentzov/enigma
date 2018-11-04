7413985a529fa055d88b4e8c498655d990e8f352
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-04 12:19:00 -0700

    Add encryption to encrypt file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,6 +1,13 @@
+require './lib/enigma'
+require 'date'
+
 read_file = ARGV[0]
 write_file = ARGV[1]
 
 file = File.open(read_file, 'r')
 message = file.read.chomp
-require 'pry'; binding.pry
+
+enigma = Enigma.new
+encryption = enigma.encrypt(message)
+
+puts "Created 'encrypted.txt' with the key #{encryption[:key]} and date #{encryption[:date]}. Encryption is #{encryption[:encryption]}."
