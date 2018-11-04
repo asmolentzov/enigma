ff02ac9743b16f7a5cd6fe5295d47032eca5b43a
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-04 14:36:07 -0700

    Adds check to initialize to see if given Date object or string

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,7 +1,13 @@
 class Offset
   
+  attr_reader :date
+  
   def initialize(date)
-    
+    if date.is_a? Date
+      @date = date.strftime('%d%m%y')
+    else 
+      @date = date
+    end
   end
   
   def create_offsets
