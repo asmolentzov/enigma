38ffd72d7cd69b355dcc54f8fa8ce8d39c5983cf
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 18:41:05 -0600

    Adds test for decrypting with no date

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -56,4 +56,13 @@ class EnigmaTest < Minitest::Test
               }
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
+  
+  def test_it_can_decrypt_a_cipher_with_no_date
+    date = Date.today.strftime('%d%m%y')
+    result = @enigma.decrypt("keder ohulw", "02715")
+    assert_equal '02715', result[:key]
+    assert_equal date, result[:date]
+    assert_equal 'keder ohulw'.length, result[:decryption].length
+    assert_instance_of String, result[:decryption]
+  end
 end
