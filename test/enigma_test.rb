b5a07bf119680532e540886c98f4cd885b61dd9d
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 18:25:08 -0600

    Adds basic decrypt test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -48,4 +48,12 @@ class EnigmaTest < Minitest::Test
     assert_equal date, result [:date]
   end
   
+  def test_it_can_decrypt_a_cipher
+    expected = {
+                decryption: "hello world",
+                key: "02715",
+                date: "040895"
+              }
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+  end
 end
