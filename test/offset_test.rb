36c6daafa5244a92873fda17a1d4a387cd5ac6d1
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-04 14:31:11 -0700

    Moves test from Shift into Offset tests

---
+++ test/offset_test.rb
@@ -0,0 +1,22 @@
+require './test/test_helper'
+require './lib/offset'
+
+class OffsetTest < Minitest::Test
+  
+  def test_it_exists
+    offset = Offset.new('121212')
+    assert_instance_of Offset, offset
+  end
+  
+  def test_it_can_create_offsets
+    offset = Offset.new('230714')
+    assert_equal [9, 7, 9, 6], offset.create_offsets
+  end
+  
+  def test_it_can_create_offset_from_date_object
+    date = Date.parse('3rd Nov 2018')
+    offset = Offset.new(date)
+    assert_equal [9, 9, 2, 4], offset.create_offsets
+  end
+  
+end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -19,16 +19,6 @@ class ShiftTest < Minitest::Test
   def test_it_can_return_key_value
     assert_equal '12345', @shift.key
   end
-    
-  def test_it_can_create_offsets
-    assert_equal [9, 7, 9, 6], @shift.create_offsets
-  end
-  
-  def test_it_can_create_offset_from_date_object
-    date = Date.parse('3rd Nov 2018')
-    shift = Shift.new('12345', date)
-    assert_equal [9, 9, 2, 4], shift.create_offsets
-  end
   
   def test_it_can_get_shifts
     assert_equal [21, 30, 43, 51], @shift.shifts
