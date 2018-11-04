442f4dcfecd369f97ab2065e95df0c78a5aed07f
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 18:08:02 -0600

    Adds decryptor tests

---
+++ test/decryptor_test.rb
@@ -0,0 +1,29 @@
+require './test/test_helper'
+require './lib/decryptor'
+
+class DecryptorTest < Minitest::Test
+  
+  def setup
+    cipher = 'keder ohulw'
+    shifts = [3, 27, 73, 20]
+    @decryptor = Decryptor.new(cipher, shifts)
+  end
+  
+  def test_it_exists
+    assert_instance_of Decryptor, @decryptor
+  end
+  
+  def test_it_can_decrypt_message
+    assert_equal 'hello world', @decryptor.decrypt
+  end
+  
+  def test_it_can_split_message_into_chunks_of_four_characters
+    expected = [['k', 'e', 'd', 'e'], ['r', ' ',  'o', 'h'], ['u', 'l', 'w']]
+    assert_equal expected, @decryptor.four_split
+    
+    decryptor = Decryptor.new('keder ohulw!', [3, 27, 73, 20])
+    expected = [['k', 'e', 'd', 'e'], ['r', ' ',  'o', 'h'], ['u', 'l', 'w', '!']]
+    assert_equal expected, decryptor.four_split
+  end
+  
+end
