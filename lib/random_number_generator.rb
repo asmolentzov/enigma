aef018377ad17ab812ac73c1f7b72bd88dffeeb6
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-04 13:23:13 -0700

    Updates module to only include string method

--- lib/random_number_generator.rb
+++ lib/random_number_generator.rb
@@ -1,13 +1,9 @@
 module RandomNumber
-  def random_number(length)
+  def random_number_string(length)
     number = ''
     length.times do 
       number += rand(9).to_s
     end
-    number.to_i
-  end
-  
-  def random_number_string(length)
-    random_number(length).to_s
+    number
   end
 end
