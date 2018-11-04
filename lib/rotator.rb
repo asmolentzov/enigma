62bfa96d3a2c8a906b8d7b42d03411470a83aa17
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-04 16:49:06 -0700

    Adds get_amount method

--- lib/rotator.rb
+++ lib/rotator.rb
@@ -21,4 +21,14 @@ class Rotator
       rotate_character(character, shifts[index])
     end
   end
+  
+  def get_amount(character, key)
+    index = @character_set.find_index(character)
+    key_index = @character_set.find_index(key)
+    difference = index - key_index
+    if difference < 0
+      difference = difference.abs + @character_set.length
+    end
+    difference
+  end
 end
