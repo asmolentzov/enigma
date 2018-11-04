95a42551e093a579e2b332620ffe10a192456e66
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-04 14:06:57 -0700

    Moves generating random number method from shift to key test

--- test/key_test.rb
+++ test/key_test.rb
@@ -5,7 +5,7 @@ require './lib/enigma'
 class KeyTest < Minitest::Test
     
     def test_it_exists
-      key = Key.new(key)
+      key = Key.new('12345')
       assert_instance_of Key, key
     end
     
@@ -26,6 +26,16 @@ class KeyTest < Minitest::Test
     def test_it_can_split_up_key
       key = Key.new('12345')
       assert_equal [12, 23, 34, 45], key.keys
+    end
+    
+    def test_it_can_generate_random_number
+      key = Key.new('12345')
+      number = key.random_number_string(5)
+      assert_instance_of String, number
+      assert_equal 5, number.length
+      assert_instance_of Integer, number.to_i
+      number_2 = key.random_number_string(5)
+      refute number == number_2
     end
   
 end
