f0216090e9be52a17c5ed9e5400e44bf8671874c
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 18:13:24 -0600

    Add decrypt method

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -1,4 +1,4 @@
-
+require './lib/rotator'
 
 class Decryptor
   
@@ -7,7 +7,16 @@ class Decryptor
     @shifts = shifts
   end
   
-
+  def decrypt
+    rotator = Rotator.new(("a".."z").to_a << " ")
+    negative_shifts = @shifts.map do |shift|
+      -shift
+    end
+    character_chunks = four_split
+    character_chunks.map do |character_chunk|
+      rotator.rotate_four(character_chunk, negative_shifts)
+    end.join
+  end
   
   def four_split
     cipher_characters = @cipher.split('')
