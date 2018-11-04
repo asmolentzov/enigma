e7d84800e9ba28e2b8634cdd9da512e17656af08
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-04 10:49:27 -0700

    Adds encrypt/decrypt class functions

--- lib/codebook.rb
+++ lib/codebook.rb
@@ -1,7 +1,11 @@
 class Codebook
   
-  def self.encrypt()
-    
+  def self.encrypt(message, shift)
+    self.new(message, shift.shifts).code
+  end
+  
+  def self.decrypt(cipher, shift)
+    self.new(cipher, shift.backwards_shifts).code
   end
   
   def initialize(phrase, shifts)
