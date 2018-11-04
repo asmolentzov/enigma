35218f7906c3676523e565a03c408b6fe8c1af89
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-03 18:18:12 -0600

    Adds get negative shifts method in instance variable

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -4,17 +4,14 @@ class Decryptor
   
   def initialize(cipher, shifts)
     @cipher = cipher
-    @shifts = shifts
+    @shifts = get_negative_shifts(shifts)
   end
   
   def decrypt
     rotator = Rotator.new(("a".."z").to_a << " ")
-    negative_shifts = @shifts.map do |shift|
-      -shift
-    end
     character_chunks = four_split
     character_chunks.map do |character_chunk|
-      rotator.rotate_four(character_chunk, negative_shifts)
+      rotator.rotate_four(character_chunk, @shifts)
     end.join
   end
   
@@ -25,6 +22,12 @@ class Decryptor
       split_cipher << cipher_characters.shift(4)
     end
     split_cipher
+  end
+  
+  def get_negative_shifts(shifts)
+    shifts.map do |shift|
+      -shift
+    end
   end
   
 end
