721b4d6fa2157ff69f133c14794d896d40b31104
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-05 20:43:50 -0700

    Adds tests for crack

--- test/crack_test.rb
+++ test/crack_test.rb
@@ -4,10 +4,34 @@ require './lib/crack'
 class CrackTest < Minitest::Test
   
   def test_it_exists
-    cipher = 'hello world end'
+    cipher = 'aldjfalksdjfla'
     date = Date.parse('5th Nov 2018')
     crack = Crack.new(cipher, date)
     assert_instance_of Crack, crack
+  end
+  
+  def test_it_has_attributes
+    cipher = 'jcufqyeitjmuglm'
+    date = Date.parse('5th Nov 2018')
+    crack = Crack.new(cipher, date)
+    assert_equal cipher, crack.cipher
+    assert_equal date, crack.date
+  end
+  
+  def test_its_key_starts_blank
+    cipher = 'jcufqyeitjmuglm'
+    date = Date.parse('5th Nov 2018')
+    crack = Crack.new(cipher, date)
+    assert_equal '', crack.key
+  end
+  
+  def test_it_can_crack_a_cipher
+    cipher = 'jcufqyeitjmuglm'
+    date = Date.parse('5th Nov 2018')
+    cracked = Crack.crack(cipher, date)
+    assert_equal 'hello world end', cracked.decryption
+    assert_equal date, cracked.date
+    assert_equal '74344', cracked.key
   end
   
 end
