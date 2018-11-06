f62186b597f5a8387f7278f2d254fc00b9b0dd93
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-06 13:35:41 -0700

    Clarifies variable names in Offset

--- lib/offset.rb
+++ lib/offset.rb
@@ -12,9 +12,9 @@ class Offset
   
   def offsets
     squared_date = @date.to_i ** 2
-    last_four = squared_date.to_s[-Enigma::NUMBER_SHIFTS,
+    last_four_date_chars = squared_date.to_s[-Enigma::NUMBER_SHIFTS,
                                   Enigma::NUMBER_SHIFTS]
-    last_four.split('').map do |char|
+    last_four_date_chars.split('').map do |char|
       char.to_i
     end
   end
