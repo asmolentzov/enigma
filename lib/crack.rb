daa9d4366753b4d4cda77b51d3c4d6f1d8cd7a73
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-05 20:44:16 -0700

    Adds tests for crack

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,7 +1,13 @@
 class Crack
   
+  attr_reader :cipher,
+              :date,
+              :key
+              
   def initialize(cipher, date)
-    
+    @cipher = cipher
+    @date = date
+    @key = ''
   end
   
   
