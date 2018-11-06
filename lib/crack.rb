0f2fb3ad18714ea3f3c2c6a04d86bb2aa2c98058
Anna Smolentzov <annasmolentzov@gmail.com>
2018-11-06 13:16:30 -0700

    Updates for switching to class method

--- lib/crack.rb
+++ lib/crack.rb
@@ -2,18 +2,25 @@ require './lib/enigma'
 require './lib/key'
 
 class Crack
-
   
-  attr_reader :cipher,
+  def self.crack(cipher, date)
+    message = ''
+    enigma = Enigma.new
+    until message[-4, 4] == ' end'
+      key = Key.new(nil).key
+      message = enigma.decrypt(cipher, key, date)[:decryption]
+    end
+    self.new(message, key, date)
+  end
+  
+  attr_reader :message,
               :date,
-              :key, 
-              :decryption
+              :key
               
-  def initialize(cipher, date)
-    @cipher = cipher
+  def initialize(message, key, date)
+    @message = message
     @date = date
-    @key = ''
-    @decryption = ''
+    @key = key
   end
   
   def crack
--- test/crack_test.rb
+++ test/crack_test.rb
@@ -4,33 +4,37 @@ require './lib/crack'
 class CrackTest < Minitest::Test
   
   def test_it_exists
-    cipher = 'aldjfalksdjfla'
+    message = 'hello world end'
     date = Date.parse('5th Nov 2018')
-    crack = Crack.new(cipher, date)
+    key = '12345'
+    crack = Crack.new(message, key, date)
     assert_instance_of Crack, crack
   end
   
   def test_it_has_attributes
-    cipher = 'jcufqyeitjmuglm'
+    message = 'hello world end'
     date = Date.parse('5th Nov 2018')
-    crack = Crack.new(cipher, date)
-    assert_equal cipher, crack.cipher
+    key = '12345'
+    crack = Crack.new(message, key, date)
+    assert_equal message, crack.message
     assert_equal date, crack.date
+    assert_equal key, crack.key
   end
   
-  def test_its_key_and_decryption_starts_blank
-    cipher = 'jcufqyeitjmuglm'
-    date = Date.parse('5th Nov 2018')
-    crack = Crack.new(cipher, date)
-    assert_equal '', crack.key
-    assert_equal '', crack.decryption
-  end
+  # def test_its_key_and_decryption_starts_blank
+  #   message = 'hello world end'
+  #   key = '12345'
+  #   date = Date.parse('5th Nov 2018')
+  #   crack = Crack.new(message, key, date)
+  #   assert_equal '', crack.key
+  #   assert_equal '', crack.decryption
+  # end
   
   def test_it_can_crack_a_cipher
     cipher = 'jcufqyeitjmuglm'
     date = Date.parse('5th Nov 2018')
     cracked = Crack.crack(cipher, date)
-    assert_equal 'hello world end', cracked.decryption
+    assert_equal 'hello world end', cracked.message
     assert_equal date, cracked.date
     assert_equal '74344', cracked.key
   end
